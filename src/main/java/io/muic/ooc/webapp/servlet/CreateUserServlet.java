18b6b371442ddc0860d58cbea0fef0b5ae9150d4
Gift-Phutatham <phutathamgift@gmail.com>
2021-07-09 01:23:45 +0700

    Added new user button and servlet for creating and updating user. Fixed path to jsp file in all servlets

--- src/main/java/io/muic/ooc/webapp/servlet/CreateUserServlet.java
+++ src/main/java/io/muic/ooc/webapp/servlet/CreateUserServlet.java
@@ -1,32 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.muic.ooc.webapp.servlet;
 
 import io.muic.ooc.webapp.Routable;
 import io.muic.ooc.webapp.service.SecurityService;
 import io.muic.ooc.webapp.service.UserService;
 
-import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-/**
- *
- * @author gigadot
- */
-public class HomeServlet extends HttpServlet implements Routable {
+public class CreateUserServlet extends HttpServlet implements Routable {
 
     private SecurityService securityService;
 
     @Override
     public String getMapping() {
-        return "/index.jsp";
+        return "/user/create";
     }
 
     @Override
@@ -38,21 +29,18 @@ public class HomeServlet extends HttpServlet implements Routable {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         boolean authorized = securityService.isAuthorized(request);
         if (authorized) {
-            // do MVC in here
             String username = (String) request.getSession().getAttribute("username");
             UserService userService = UserService.getInstance();
-            request.setAttribute("currentUser", userService.findByUsername(username));
-            request.setAttribute("users", userService.findAll());
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
+            request.setAttribute("user", userService.findByUsername(username));
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/create.jsp");
             rd.include(request, response);
-            // removing attributes as soon as they are used is known as flash session
             request.getSession().removeAttribute("hasError");
             request.getSession().removeAttribute("message");
         } else {
-            // just add some extra precaution to delete these two attributes
             request.removeAttribute("hasError");
             request.removeAttribute("message");
             response.sendRedirect("/login");
         }
     }
+
 }
