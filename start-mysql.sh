ba867a6105c66d443ed3c13ca3493a1bf6978fda
Gift-Phutatham <phutathamgift@gmail.com>
2021-07-12 03:01:16 +0700

    Fixed database connection

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d -p 5555:3306 mysql
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d -p 127.0.0.1:5555:3306 mysql
